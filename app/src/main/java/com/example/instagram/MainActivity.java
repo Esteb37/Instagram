8e6090b17831d527663ef1575a60e4de0e98ca88
Esteb37 <esteban37padilla@gmail.com>
2021-07-06 14:51:13 -0500

    Post querying

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.instagram.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding app;
+    public static final  String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         });
 
+        queryPosts();
+    }
+
+    private void queryPosts(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e!=null){
+                    Log.d(TAG,"Issue with getting posts",e);
+                    return;
+                }
+
+                for(Post post : posts){
+                    Log.i(TAG,"Post: "+post.getDescription()+", username: "+post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
